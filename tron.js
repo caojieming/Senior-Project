7c23073809c6a841f74dbc4d5ff9cff6ebcb2349
James Cao <jamescao@James-MacBook-Pro.local>
2020-05-13 20:23:23 -0400

    made the player class and added functions

--- tron.js
+++ tron.js
@@ -1,7 +1,60 @@
 console.log("moo");
 
-const canvas = document.getElementById('canvas');
+const canvas = document.getElementById('canvas');//"const" cannot change
 const ctx = canvas.getContext('2d');
+const unit = 15;
 
-ctx.fillStyle = 'green';
-ctx.fillRect(10, 10, 150, 100);//startx, starty, endx, endy
+class Player{
+
+  constructor(x, y, color){
+    this.color = color || '#fff';
+    this.sX = x;//starting positions
+    this.sY = y;
+    this.cX = x;//current positions
+    this.cY = y;
+    this.key = '';
+    this.direction = '';
+    this.dead = false;
+
+    this.constructor.counter = (this.constructor.counter || 0) + 1;
+    this.id = this.constructor.counter;
+
+    Player.playerList.push(this);
+  };
+
+};
+
+Player.playerList = [];
+let p1 = new Player(unit*5, unit*5, '#75A4FF');//"let" is temporary within its brackets
+let p2 = new Player(unit*50, unit*50, '#FF5050');
+
+function setKeys(key, player, up, down, left, right){
+
+}
+
+function handleKeys(event){
+
+}
+
+function getPlayableCells(canvas, unit){
+
+}
+
+function drawBackground(){
+
+}
+
+function drawStartingPositions(players){
+
+}
+
+function draw(){
+
+}
+
+function resetGame(){
+
+}
+
+// ctx.fillStyle = 'green';
+// ctx.fillRect(10, 10, 150, 100);//startx, starty, endx, endy
